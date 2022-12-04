75c2a99819d3376d27bc71738857e7d56974b93f
wagatsuma.yuichi <firefly.of.april@gmail.com>
2022-12-04 23:30:14 +0900

    [add] #5 Storybookでcssではなくscssファイルを使用できるよう設定した

--- app/frontend/.storybook/main.js
+++ app/frontend/.storybook/main.js
@@ -2,27 +2,42 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  "staticDirs": ['public'],
-  "webpackFinal": async (config) => {
+  staticDirs: ['public'],
+  webpackFinal: async (config) => {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, '../tsconfig.json')
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
       }),
-    ];
-
+    ]
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
     return config
+  },
+  core: {
+    builder: 'webpack5',
   },
 }
