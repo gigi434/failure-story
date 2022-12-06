062c0bf6d241a6aa02ed0c7716b69ce353dfddf4
wagatsuma.yuichi <firefly.of.april@gmail.com>
2022-12-06 19:57:15 +0900

    [add] #5 Webpackの機能について説明を追加した

--- app/frontend/.storybook/main.js
+++ app/frontend/.storybook/main.js
@@ -12,27 +12,38 @@ module.exports = {
   core: {
     builder: '@storybook/builder-webpack5',
   },
-  staticDirs: ['public'],
+  // Webサーバーで公開する静的ファイルを格納するディレクトリを指定する
+  staticDirs: ['../public'],
   webpackFinal: async (config) => {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
+        // storybookにbaseURLはsrcディレクトリであることを読み込ませる
         configFile: path.resolve(__dirname, '../tsconfig.json'),
       }),
     ]
+    // Webpackは本来JSしか使えないが、CSSや画像をJSのオブジェクトにしてWebpackで扱えるようにするのがローダー
+    // ローダーなどのモジュールの設定をプロパティのリストに追加していく
     config.module.rules.push({
+      // 正規表現で拡張子が.scssに該当するファイルである際にトランスパイルするプロパティ
       test: /\.scss$/,
+      // 使用するローダーを指定するプロパティ
       use: [
+        // バンドルされたCSSをHTMLでスタイルシートとして読み込ませるようにする
         'style-loader',
         {
+          // cssファイルをCommonJSに変換する
           loader: 'css-loader',
           options: {
             modules: {
+              // 一致する全てのファイルでCSSモジュールを有効にする
               auto: true,
             },
           },
         },
+        // scssファイルをcssファイルにコンパイルする
         'sass-loader',
       ],
+      // 指定されたディレクトリ内に存在する正規表現に該当したファイルをトランスパイルする
       include: path.resolve(__dirname, '../'),
     })
     return config
